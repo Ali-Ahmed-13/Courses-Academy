07e3116a7ecde575b2027c37b93cfae47579b887
ana-TIGO <alio123alio1239o@gmail.com>
2026-01-26 11:18:05 +0200

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -6,13 +6,13 @@ import { ClerkProvider } from '@clerk/nextjs';
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
-  display: 'optional',
+  display: 'swap',
 });
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
-  display: 'optional',
+  display: 'swap',
 });
 
 export const metadata = {
