d31eb9776071603a124375be791f54f6d14e5aa8
Ali-Ahmed-13 <alio123alio1239o@gmail.com>
2026-04-07 13:29:31 +0200

    Quiz

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
   webpack: (config, options) => {
     config.resolve.modules.push(path.join(__dirname, 'app'));
     return config;
-  },
+  }
 };
 
 export default nextConfig;
