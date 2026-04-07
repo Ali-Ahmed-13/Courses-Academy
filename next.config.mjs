80f57554d41c55a626c8de60de320039f5c6a036
Ali-Ahmed-13 <alio123alio1239o@gmail.com>
2026-04-07 14:07:28 +0200

    Quiz

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').Config} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'img.youtube.com',
-  //       port: '',
-  //       pathname: '/vi/**',
-  //     },
-  //   ],
-  // },
-  // webpack: (config, options) => {
-  //   config.resolve.modules.push(path.join(__dirname, 'app'));
-  //   return config;
-  // }
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.youtube.com',
+        port: '',
+        pathname: '/vi/**',
+      },
+    ],
+  },
+  webpack: (config, options) => {
+    config.resolve.modules.push(path.join(__dirname, 'app'));
+    return config;
+  }
 };
 
 export default nextConfig;
